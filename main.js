841754d37db81762370abea208dab7dea5c21ed0
Lucas Mendes <luckasmendys72@hotmail.com>
2022-12-14 10:31:44 -0300

    finalizando a implementação da div que mostra os certificados

--- main.js
+++ main.js
@@ -7,12 +7,13 @@ botaoAbreCertificado.addEventListener('click', apareceJanela);
 var meuMain = document.getElementsByTagName('main');
 
 function apareceJanela() {
-    divCertificados.style.display = "block"
+    divCertificados.style.display = "block";
     meuMain[0].classList.add("teste");
 }
 
 botaoFechaCertificado.addEventListener('click', fechaJanela)
 
 function fechaJanela(){
-    
+    divCertificados.style.display = "none";
+    meuMain[0].classList.remove("teste");
 }
